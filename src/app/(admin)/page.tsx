821eb016c2dce0f6764d8a5b3ebc2b2ccf1c127a
nasswcksn <anaswicaksonoanas@gmail.com>
2025-11-12 15:08:22 +0700

    rename header

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -9,8 +9,8 @@ import DemographicCard from "@/components/ecommerce/DemographicCard";
 
 export const metadata: Metadata = {
   title:
-    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Home for TailAdmin Dashboard Template",
+    "AnalysMU | Workshop Social Media Analysis Final Project",
+  description: "This is AnalysMU Home Dashboard",
 };
 
 export default function Ecommerce() {
