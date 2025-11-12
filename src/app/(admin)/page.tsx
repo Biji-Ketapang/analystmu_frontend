6ef805e7dea4da8abeddb0eec8578eae4fc559e7
nasswcksn <anaswicaksonoanas@gmail.com>
2025-11-12 16:18:14 +0700

    Update UI/UX

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -6,26 +6,41 @@ import MonthlySalesChart from "@/components/ecommerce/MonthlySalesChart";
 import StatisticsChart from "@/components/ecommerce/StatisticsChart";
 import RecentOrders from "@/components/ecommerce/RecentOrders";
 import DemographicCard from "@/components/ecommerce/DemographicCard";
+import PostTypeAnalysis from "@/components/ecommerce/PostTypeAnalysis";
+import EngagementByPostType from "@/components/ecommerce/EngagementByPostType";
+import TopEngagementPosts from "@/components/ecommerce/TopEngagementPosts";
 
 export const metadata: Metadata = {
   title:
-    "AnalysMU | Workshop Social Media Analysis Final Project",
+    "AnalysMU | Social Media Analysis Dashboard",
   description: "This is AnalysMU Home Dashboard",
 };
 
 export default function Ecommerce() {
   return (
     <div className="grid grid-cols-12 gap-4 md:gap-6">
-      <div className="col-span-12 space-y-6 xl:col-span-7">
+      {/* Metrics Cards */}
+      <div className="col-span-12">
         <EcommerceMetrics />
+      </div>
 
+      {/* Post Type Analysis - Full Width */}
+      <div className="col-span-12">
+        <PostTypeAnalysis />
+      </div>
+
+      {/* Engagement by Post Type & Monthly Engagement */}
+      <div className="col-span-12 space-y-6 xl:col-span-7">
+        <EngagementByPostType />
         <MonthlySalesChart />
       </div>
 
+      {/* Top Posts & Target */}
       <div className="col-span-12 xl:col-span-5">
-        <MonthlyTarget />
+        <TopEngagementPosts />
       </div>
 
+      {/* Additional Charts */}
       <div className="col-span-12">
         <StatisticsChart />
       </div>
