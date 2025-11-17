e91920e16b0a2b781ce782bece09603577a49226
nasswcksn <anaswicaksonoanas@gmail.com>
2025-11-17 19:05:03 +0700

    update information

--- src/components/user-profile/UserAddressCard.tsx
+++ src/components/user-profile/UserAddressCard.tsx
@@ -28,7 +28,7 @@ export default function UserAddressCard() {
                   Country
                 </p>
                 <p className="text-sm font-medium text-gray-800 dark:text-white/90">
-                  United States
+                  Indonesia
                 </p>
               </div>
 
@@ -37,7 +37,7 @@ export default function UserAddressCard() {
                   City/State
                 </p>
                 <p className="text-sm font-medium text-gray-800 dark:text-white/90">
-                  Phoenix, Arizona, United States.
+                  Surabaya, Jawa Timur.
                 </p>
               </div>
 
@@ -55,7 +55,7 @@ export default function UserAddressCard() {
                   TAX ID
                 </p>
                 <p className="text-sm font-medium text-gray-800 dark:text-white/90">
-                  AS4568384
+                  A54568384
                 </p>
               </div>
             </div>
@@ -99,12 +99,12 @@ export default function UserAddressCard() {
               <div className="grid grid-cols-1 gap-x-6 gap-y-5 lg:grid-cols-2">
                 <div>
                   <Label>Country</Label>
-                  <Input type="text" defaultValue="United States" />
+                  <Input type="text" defaultValue="Indonesia" />
                 </div>
 
                 <div>
                   <Label>City/State</Label>
-                  <Input type="text" defaultValue="Arizona, United States." />
+                  <Input type="text" defaultValue="Surabaya, Jawa Timur." />
                 </div>
 
                 <div>
@@ -114,7 +114,7 @@ export default function UserAddressCard() {
 
                 <div>
                   <Label>TAX ID</Label>
-                  <Input type="text" defaultValue="AS4568384" />
+                  <Input type="text" defaultValue="A54568384" />
                 </div>
               </div>
             </div>
